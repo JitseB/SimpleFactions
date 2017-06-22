c96b60b38e7fb7c1cd0fe403d8317227f8771605
Jitse B <Jitse@operamail.com>
2017-06-22 16:07:23 +0200

    First commit.

---
+++ src/net/jitse/simplefactions/SimpleFactions.java
@@ -0,0 +1,14 @@
+package net.jitse.simplefactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Jitse on 22-6-2017.
+ */
+public class SimpleFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
