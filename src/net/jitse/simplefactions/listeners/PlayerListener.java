50e6ce8e0af2045f6d7cf332286454d7686de0cb
Jitse B <Jitse@operamail.com>
2017-06-23 19:15:03 +0200

    More stuff.

--- src/net/jitse/simplefactions/listeners/PlayerListener.java
+++ src/net/jitse/simplefactions/listeners/PlayerListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -13,7 +14,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
  */
 public class PlayerListener implements Listener {
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerMove(PlayerMoveEvent event){
         Player player = event.getPlayer();
         Location from = event.getFrom();
@@ -21,6 +22,8 @@ public class PlayerListener implements Listener {
         if(from.getChunk().getX() != to.getChunk().getX() || from.getChunk().getZ() != to.getChunk().getZ())
             Bukkit.getPluginManager().callEvent(new PlayerChangeChunkEvent(player, from.getChunk(), to.getChunk()));
     }
+
+
 
     @EventHandler
     public void onPlayerChunkChange(PlayerChangeChunkEvent event){
