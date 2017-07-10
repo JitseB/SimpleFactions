5c9a161cbc6df390e7ba4479f2283ac8d7435eda
Jitse B <Jitse@operamail.com>
2017-07-10 22:25:18 +0200

    More stuff.

--- src/net/jitse/simplefactions/managers/Settings.java
+++ src/net/jitse/simplefactions/managers/Settings.java
@@ -20,7 +20,7 @@ public class Settings {
     public static final ChatColor ALLY_FACTION_COLOR = ChatColor.LIGHT_PURPLE;
     public static final int MAX_MEMBERS = 50;
     public static final int INVITE_EXPIRE_TIME = 60;
-    public static final int MAX_PROXY_PLAYERS = 500;
+    public static final int MAX_PROXY_PLAYERS = 450;
 
     public static final String FACTION_ALREADY_OWNS_LAND = "&cYour faction already owns this land.";
     public static final String AUTO_CLAIMING_ENABLED = "&aAuto-Claiming is now enabled, you can start walking around now.";
